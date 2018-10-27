e169a653860dc541a2901f808aad3acdfc1a8fa2
oknkc8 <oknkc8@gmail.com>
2018-10-27 10:13:21 +0900

    upload xv6

---
+++ xv6/prac4.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "user.h"
+
+int main(){
+	char *argv[2];
+	argv[0]="cat";
+	argv[1]=0;
+
+	if(fork()==0){
+		close(0);
+		open("input.txt",0);
+		exec("cat",argv);
+	}
+	else wait();
+	exit();
+}
